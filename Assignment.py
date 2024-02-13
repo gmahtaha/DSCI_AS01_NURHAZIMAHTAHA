5123c93490cfac037c4d05e64cf22c9c8417e0ed
gmahtaha <157557995+gmahtaha@users.noreply.github.com>
2024-02-13 19:29:50 +0800

    Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -5,54 +5,74 @@ import seaborn as sns
 from streamlit_option_menu import option_menu
 from streamlit_extras.metric_cards import style_metric_cards
 
-
 # to set the page into wide 
 st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded")
 
+
+# to read data from dataset
 df = pd.read_csv("SupermarketSales.csv")
 
+# to show title in the center
+st.markdown("<h1 style='text-align: center;'>SUPERMARKET SALES DASHBOARD</h1>", unsafe_allow_html=True)
 
-with st.sidebar:
-       st.subheader ("SUPERMARKET SALES 🛒" )
-       selected = option_menu (
-              menu_title = "main menu" , 
-              options = ['Supermarket Sales Dashboard' , 'General Dashboard']
+# for the option dashboar
+selected = option_menu (
+        menu_title = None, 
+        options = ['Supermarket Sales Dashboard' , 'General Dashboard'],
+        orientation= "horizontal",
        )
 
 if selected == "Supermarket Sales Dashboard" :
-    hi ,hi1 = st.columns(2)
-    with hi :
-        st.title('SUPERMARKET SALES DASHBOARD') 
+     
+    # filterisations : 
+    st.sidebar.title('SELECT THE FILTERS')
 
-    with hi1 :
-        cont = st.container()
-        with cont : 
+    # filter for graph 1
+    st.sidebar.write ("**Total Transaction by Customer's Filter**")
+    gender = st.sidebar.multiselect('Select the Gender:', df['Gender'].unique())
+    products = st.sidebar.selectbox('Select the Products:', df['Product line'].unique())
+    graph1 = df[(df['Gender'].isin( gender) )& (df['Product line'] == products)]
 
-            city = st.selectbox('Select a City to view respective stores data',
-                                ['All', 'Yangon', 'Naypyitaw', 'Mandalay'])
-            product = st.selectbox('Select a product', ['All'] + list(df['Product line'].unique()))
+    #filter for graph 2 
+    st.sidebar.write ("**Total Transaction by Day's Filter**")
+    city = st.sidebar.radio('Select the City:', df['City'].unique())
+    prod = st.sidebar.multiselect('Select the Products:', df['Product line'].unique())
+    graph2 = df[(df['City'] == city ) & (df['Product line'].isin(prod))]
 
-            if city == 'All' and product == 'All':
-                filtered_data = df # display all
+    #filter for graph 3
+    st.sidebar.write ("**Total Transaction by Month's Filter**")
+    cit = st.sidebar.selectbox('Select the City:', df['City'].unique())
+    payment = st.sidebar.radio('Select the Payment:', df['Payment'].unique())
+    graph3 = df[(df['City'] == city ) & (df['Payment'] == payment)]
 
-            elif city == 'All':
-                filtered_data = df[df['Product line'] == product]
-                
-            elif product == 'All':
-                filtered_data = df[df['City'] == city]
+     #filter for graph 4
+    st.sidebar.write ("**Total Transaction by Payment Method's Filter**")
+    customer = st.sidebar.selectbox('Select the Customer Type:', df['Customer type'].unique())
+    month  = st.sidebar.selectbox('Select the Month:', df['Month'].unique())
+    graph4 = df[(df['Customer type'] == customer ) & (df['Month'] == month)]
 
-            else:
-                filtered_data = df[(df['City'] == city) & (df['Product line'] == product)]
+    #filter for graph 5
+    st.sidebar.write ("**Total Transaction by Hour's Filter**")
+    day = st.sidebar.multiselect('Select the Day in a week:', df['Day'].unique())
+    gen  = st.sidebar.multiselect('Select the gender:', df['Gender'].unique())
+    graph5 = df[(df['Day'].isin(day) ) & (df['Gender'].isin(gen))]
+
+    #filter for graph 6
+    st.sidebar.write ("**Total Transaction by Gender's Filter**")
+    quantity = st.sidebar.selectbox('Select the Quantiy sold:', df['Quantity'].unique())
+    pro  = st.sidebar.radio('Select the Product line:', df['Product line'].unique())
+    graph6 = df[(df['Quantity'] == quantity ) & (df['Product line'] == (pro))]
 
 
+    #KPI calculations 
 
-        #calculations 
-    Total_sales = int (filtered_data['Total'].sum())
-    Total_Gross_Income = int(filtered_data['gross income'].sum())
-    Average_rating = round(filtered_data['Rating'].mean(), 1)
-    Total_sold_unit = int (filtered_data['Quantity'].sum())
+    st.write("**Sales Performance throughout 3 Months**")
+    Total_sales = int (df['Total'].sum())
+    Total_Gross_Income = int(df['gross income'].sum())
+    Average_rating = round(df['Rating'].mean(), 1)
+    Total_sold_unit = int (df['Quantity'].sum())
 
     col1 , col2 , col3 , col4 = st.columns(4)
 
@@ -67,121 +87,241 @@ if selected == "Supermarket Sales Dashboard" :
     style_metric_cards(border_left_color="#0C0C0C")  #styling purposes
 
 
-
-    left_column, right_column , end_column = st.columns(3)
+    # for the first row ( graph )
+    left_column, right_column = st.columns(2)
 
     with left_column:
-        customer = filtered_data.groupby('Customer type')['Total'].sum()
-        fig, ax = plt.subplots()
-        ax.pie(customer, labels=customer.index, autopct='%1.1f%%', startangle=90, wedgeprops=dict(width=0.4), colors=sns.color_palette('crest'))
-        plt.axis('equal') 
-        plt.title('Total Transaction by Customer Type')
-        st.pyplot(fig)
+        st.write("**Sales Performance Graphs**")
+
+        if not graph1.empty :
+            customer = graph1.groupby('Customer type')['Total'].sum()
+            fig, ax = plt.subplots()
+            ax.pie(customer, labels=customer.index, autopct='%1.1f%%', startangle=90, wedgeprops=dict(width=0.4), colors=sns.color_palette('crest'))
+            plt.axis('equal') 
+            plt.title('Total Transaction by Customer Type')
+            st.pyplot(fig)
+        else:
+            st.write("No data available for the selected filters.")
 
     with right_column:
-            fig, ax = plt.subplots()
-            daily_sales = filtered_data.groupby('Day')['Total'].mean().reset_index()
-            sns.barplot(x='Day', y='Total', data=daily_sales, palette='crest')  
-            plt.xlabel('Day')
-            plt.ylabel('Total Sales')
-            plt.title('Total Transaction by Day')
-            plt.gca().yaxis.set_major_formatter('${:,.0f}'.format)
-            st.pyplot(fig)
-
-    with end_column :
-            fig, ax = plt.subplots()
-            monthly_sales = filtered_data.groupby('Month')['Total'].mean().reset_index()
-            sns.lineplot(x='Month', y='Total', data=monthly_sales, marker='o', color='skyblue', linewidth=2, label='Total Sales')
-            plt.fill_between(monthly_sales['Month'], monthly_sales['Total'], color='skyblue', alpha=0.3)
-            plt.xlabel('Month')
-            plt.ylabel('Total Sales')
-            plt.title('Total Transaction by Month')
-            plt.gca().yaxis.set_major_formatter('${:,.0f}'.format)
-            plt.legend()
-            st.pyplot(fig)
-
-
-    left, right , end = st.columns(3)
-
-    with left:
-            fig , ax = plt.subplots()
-            sns.barplot(data = filtered_data , x='Payment', y='Total', errorbar= None , palette= 'crest')
-            plt.xlabel('Payment Method')
-            plt.ylabel('Total Sales')
-            plt.title('Total Transaction by Payment Method')
-            plt.gca().yaxis.set_major_formatter('${:,.0f}'.format)
-            st.pyplot(fig)
-
-    with right : 
-            fig , ax = plt.subplots()
-            Payment = filtered_data.groupby('Hour')['Total'].mean().reset_index()
-            sns.lineplot(x='Hour', y='Total', data= Payment, marker='o', palette= 'crest', linewidth=2)
-            plt.xlabel('Hour')
-            plt.ylabel('Total Sales')
-            plt.title('Total Transaction by Hour')
-            plt.gca().yaxis.set_major_formatter('${:,.0f}'.format)
-            st.pyplot(fig)
-
-    with end :
-            fig, ax = plt.subplots()
-            total_sales_by_gender = filtered_data.groupby('Gender')['Total'].sum()
-            ax.pie(total_sales_by_gender, labels=total_sales_by_gender.index, autopct='%1.1f%%', startangle=90, colors=sns.color_palette('crest'))
-            plt.axis('equal')  
-            plt.title('Total Transaction by Gender')
-            st.pyplot(fig)
-
-
-#Dashboard 2  
-if selected == "General Dashboard" :
-        st.title ("General Dashboard")
-      
-        left_column, right_column, ending_column = st.columns(3)
-
-        with left_column:
+        
+        if not graph2.empty:
                 fig, ax = plt.subplots()
-                rating = df.groupby('City')['Rating'].sum()
-                ax.pie(rating, labels=rating.index, autopct='%1.1f%%', startangle=90, colors=sns.color_palette('crest'))
-                plt.axis('equal')  
-                plt.title('Rating by City')
+                sns.barplot(data= graph2, x='Day', y='Total', errorbar= None , palette='crest')  
+                plt.xlabel('Day')
+                plt.ylabel('Total Sales')
+                plt.title('Total Transaction by Day')
+                plt.gca().yaxis.set_major_formatter('${:,.0f}'.format)
                 st.pyplot(fig)
 
-        with right_column:
+        else:
+                 st.write("No data available for the selected filters.")
+
+    c1, c2 = st.columns(2)
+
+    with c1 :
+            
+            if not graph3.empty:
+
+                fig, ax = plt.subplots()
+                monthly_sales = graph3.groupby('Month')['Total'].mean().reset_index()
+                sns.lineplot(x='Month', y='Total', data=monthly_sales, marker='o', color='skyblue', linewidth=2, label='Total Sales')
+                plt.fill_between(monthly_sales['Month'], monthly_sales['Total'], color='skyblue', alpha=0.3)
+                plt.xlabel('Month')
+                plt.ylabel('Total Sales')
+                plt.title('Total Transaction by Month')
+                plt.gca().yaxis.set_major_formatter('${:,.0f}'.format)
+                plt.legend()
+                st.pyplot(fig)
+
+            else:
+                st.write("No data available for the selected filters.")
+
+
+    with c2:
+            
+            if not graph4.empty:
+
                 fig , ax = plt.subplots()
-                sns.barplot(data = df , x='City', y='Total', errorbar= None , palette= 'crest')
+                sns.barplot(data = graph4 , x='Payment', y='Total', errorbar= None , palette= 'crest')
+                plt.xlabel('Payment Method')
+                plt.ylabel('Total Sales')
+                plt.title('Total Transaction by Payment Method')
+                plt.gca().yaxis.set_major_formatter('${:,.0f}'.format)
+                st.pyplot(fig)
+
+            else:
+                st.write("No data available for the selected filters.")
+
+    no1, no2  = st.columns(2)
+    with no1 : 
+            
+            if not graph5.empty:
+
+                fig , ax = plt.subplots()
+                Payment = graph5.groupby('Hour')['Total'].mean().reset_index()
+                sns.lineplot(x='Hour', y='Total', data= Payment, marker='o', palette= 'crest', linewidth=2)
+                plt.xlabel('Hour')
+                plt.ylabel('Total Sales')
+                plt.title('Total Transaction by Hour')
+                plt.gca().yaxis.set_major_formatter('${:,.0f}'.format)
+                st.pyplot(fig)
+
+            else:
+                st.write("No data available for the selected filters.")
+
+
+    with no2 :
+            if not graph6.empty:
+
+                fig, ax = plt.subplots()
+                total_sales_by_gender = graph6.groupby('Gender')['Total'].sum()
+                ax.pie(total_sales_by_gender, labels=total_sales_by_gender.index, autopct='%1.1f%%', startangle=90, colors=sns.color_palette('crest'))
+                plt.axis('equal')  
+                plt.title('Total Transaction by Gender')
+                st.pyplot(fig)
+
+            else:
+                st.write("No data available for the selected filters.")
+
+
+
+
+if selected == "General Dashboard" :
+    # filterisations : 
+    st.sidebar.title('SELECT THE FILTERS')
+
+
+    #KPI calculations 
+
+    st.write("**Sales Performance throughout 3 Months**")
+    Total_sales = int (df['Total'].sum())
+    Total_Gross_Income = int(df['gross income'].sum())
+    Average_rating = round(df['Rating'].mean(), 1)
+    Total_sold_unit = int (df['Quantity'].sum())
+
+    col1 , col2 , col3 , col4 = st.columns(4)
+
+    col1.metric("Total Sales", f"$ {Total_sales}")
+
+    col2.metric("Total Gross Income", f"$ {Total_Gross_Income}")
+
+    col3.metric("Total Sold Unit", Total_sold_unit)
+
+    col4.metric("Average ", Average_rating ,)
+
+    style_metric_cards(border_left_color="#0C0C0C")  #styling purposes
+
+
+    # filter for graph 1
+    st.sidebar.write ("**Rating by City's Filter**")
+    pl = st.sidebar.selectbox('Select the Products:', df['Product line'].unique())
+    ct = st.sidebar.selectbox('Select the Customer Type:', df['Customer type'].unique())
+    first = df[(df['Product line'] == (pl) )& (df['Customer type'] == ct)]
+
+    #filter for graph 2 
+    st.sidebar.write ("**Sales by City's Filter**")
+    month  = st.sidebar.radio('Select the Month:', df['Month'].unique())
+    products = st.sidebar.multiselect('Select the Products:', df['Product line'].unique())
+    second = df[(df['Month'] == month ) & (df['Product line'].isin(products))]
+
+    #filter for graph 3
+    st.sidebar.write ("**Distribution of Gender's Filter**")
+    city = st.sidebar.selectbox('Select the City:', df['City'].unique())
+    quantity = st.sidebar.selectbox('Select the Quantiy sold:', df['Quantity'].unique())
+    third = df[(df['City'] == city ) & (df['Quantity'] == quantity)]
+
+     #filter for graph 4
+    st.sidebar.write ("**Total product Distribution's Filter**")
+    days = st.sidebar.multiselect('Select the Day in a week:', df['Day'].unique())
+    sex  = st.sidebar.multiselect('Select the gender:', df['Gender'].unique())
+    forth = df[(df['Day'].isin(days) ) & (df['Gender'].isin(sex))]
+
+    #filter for graph 5
+    st.sidebar.write ("**Customer type by gender's Filter**")
+    days = st.sidebar.selectbox('Select the Day in a week:', df['Day'].unique())
+    branch = st.sidebar.radio('Select the City:', df['City'].unique())
+    fifth = df[(df['Day'] == (days) ) & (df['City'] == branch)]
+
+    #filter for graph 6
+
+
+    left_column, right_column= st.columns(2)
+
+    with left_column:
+            
+            if not first.empty :
+                fig, ax = plt.subplots()
+                rating = first.groupby('City')['Rating'].sum().reset_index()  # Reset index to use 'City' column in barplot
+                sns.barplot(data=rating, x='City', y='Rating', palette='crest')
+                plt.xlabel('City')
+                plt.ylabel('Rating')
+                plt.title('Rating by City')
+                st.pyplot(fig)
+            else:
+                st.write("No data available for the selected filters.")
+
+
+    with right_column:
+            
+            if not second.empty :
+
+                fig , ax = plt.subplots()
+                sns.barplot(data = second , x='City', y='Total', errorbar= None , palette= 'crest')
                 plt.xlabel('City')
                 plt.ylabel('Total Sales')
                 plt.title('Sales by City')
                 plt.gca().yaxis.set_major_formatter('${:,.0f}'.format)
                 st.pyplot(fig)
-                
-        with ending_column:
+
+            else:
+                st.write("No data available for the selected filters.")
+
+
+    left, right = st.columns(2)
+            
+    with left:
+
+        if not third.empty :
+
             fig, ax = plt.subplots()
-            gender_counts = df['Gender'].value_counts()
+            gender_counts = third['Gender'].value_counts()
             ax.pie(gender_counts, labels=gender_counts.index, autopct='%1.1f%%', startangle=90, wedgeprops=dict(width=0.4), colors=sns.color_palette('crest'))
             plt.title('Distribution of Gender')
             st.pyplot(fig)
 
-        left, right , end = st.columns(3)
+        else:
+                st.write("No data available for the selected filters.")
 
-        with left:
-                fig , ax = plt.subplots()
-                products= df['Product line'].value_counts()
-                sns.barplot(x=products.values, y=products.index, palette='crest')
-                plt.ylabel('Products')
-                plt.xlabel('Count (Products)')
+    with right:
+            
+            if not forth.empty :
+
+                fig, ax = plt.subplots()
+                products = forth['Product line'].value_counts()
+                ax.pie(products, labels=products.index, autopct='%1.1f%%', startangle=90, colors=sns.color_palette('crest'))
+                plt.axis('equal')
                 plt.title('Total Products Distribution')
                 st.pyplot(fig)
 
-        with right:
+
+            else:
+                st.write("No data available for the selected filters.")
+    
+    col1, col2 = st.columns(2)
+
+    with col1:
+            
+            if not fifth.empty:
+
                 fig, ax = plt.subplots()
                 custom_palette = {'Male': '#289E8F', 'Female': '#1C3269'}
-                sns.countplot(data=df,x=df['Customer type'],hue =df['Gender'], palette=custom_palette)
+                sns.countplot(data= fifth,x=fifth['Customer type'],hue =fifth['Gender'], palette=custom_palette)
                 plt.title('Customer type by Gender')
                 st.pyplot(fig)
 
-        with end:
-            fig, ax = plt.subplots()
-            custom_palette = {'Male': '#289E8F', 'Female': '#1C3269'}
-            sns.countplot(data=df,x=df['Customer type'],hue =df['Gender'], palette=custom_palette)
-            plt.title('Customer type by Gender')
-            st.pyplot(fig)
+            else:
+                st.write("No data available for the selected filters.")
+
+    
+
